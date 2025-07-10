fa6218a0d73d7bcc727e0d55775cc762b47e7296
unknown <mohamed.aymn.mosalam@gmail.com>
2025-07-10 14:23:50 +0300

    infra module

---
+++ infra/eks.tf
@@ -0,0 +1,23 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.4"
+
+  cluster_name    = "devops-kit-cluster"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
+    }
+  }
+
+  tags = {
+    Project = "devops-kit"
+  }
+}
