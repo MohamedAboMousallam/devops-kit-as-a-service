8417d258e9933f8a17665abcdcbd223e4e40ff87
unknown <mohamed.aymn.mosalam@gmail.com>
2025-08-10 16:16:08 +0300

    adding Pod name print message for app to showcase LB

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
 from prometheus_flask_exporter import PrometheusMetrics
+import os
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+pod_name = os.environ.get('HOSTNAME', 'unknown-pod')
 
 @app.route("/")
 def index():
-    return jsonify({"message": "DevOps Kit API is running!"})
+    return jsonify({"message": "DevOps Kit API is running! this is a hello message from Pod:", "pod_name": pod_name})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
